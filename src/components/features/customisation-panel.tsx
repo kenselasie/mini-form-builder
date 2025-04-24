71b8802fdadb543e706a34e7cab96c53bd4f02b3
kenselasie <kenselasie@gmail.com>
2025-04-24 18:37:21 +0200

    added flex wrap

--- src/components/features/customisation-panel.tsx
+++ src/components/features/customisation-panel.tsx
@@ -47,7 +47,7 @@ const CustomisationPanel = ({ onFormDataChange }: CustomisationPanelProps) => {
             setFormData((prev) => ({ ...prev, backgroundColor: value }))
           }
         >
-          <div className="flex items-center space-x-4">
+          <div className="flex flex-wrap items-center gap-4">
             <RadioGroupItem
               data-testid="color-white"
               value="white"
